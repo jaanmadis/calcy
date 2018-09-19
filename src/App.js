71996969e22e5ccea5a2bccdc4fc0fffc304e26f
Jaan-Madis Uusvali <jaan.madis.uusvali@gmail.com>
2018-09-19 08:58:01 -0700

    Menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import Equation from './Core/Equation/Equation';
+import Menu from './Layout/Menu/Menu';
 
 class App extends Component {
     render() {
         return (
-            <Equation />
+            <div>
+                <Menu />
+                <Equation />
+            </div>
         );
     }
 }
---
+++ src/Layout/Menu/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const menu = () => {
+    return (
+        <ul>
+            <li>
+                1
+            </li>
+            <li>
+                2
+            </li>
+            <li>
+                3
+            </li>
+        </ul>
+    );
+};
+
+export default menu;
